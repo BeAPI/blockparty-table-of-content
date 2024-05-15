d01d0b89cc78757098855e5a7e22c5bda380be37
Marie Comet <mcomet@beapi.fr>
2024-05-15 12:28:51 +0200

    add List styles

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n', 'wp-rich-text'), 'version' => '8c9d544b616b166304cf');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n', 'wp-rich-text'), 'version' => '1b4ba710f20d8709f597');
