0cf3ae6392e0893144816d93df12ccfaec6c6d62
Clement Boirie <cboirie@beapi.fr>
2024-06-18 09:55:00 +0000

    Release version 1.0.0

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n', 'wp-rich-text'), 'version' => '2d968cdf389ec3e70a48');
