ba8056698c820c8aa4dcb65b0b6d793adef48642
Marie Comet <mcomet@beapi.fr>
2024-06-20 10:10:18 +0000

    Release version 1.0.1

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n', 'wp-rich-text'), 'version' => '5df4c073d8810bc33b0d');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '2b9d3f24d6feb4ef1221');
